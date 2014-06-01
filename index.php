4b98487a97dd452f3b40d05aec968b8e295cc9bf
we6jbo <we6jbo@gmail.com>
2014-06-01 16:49:37 -0700

    Create index.php
    This is the index.php file.

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+echo "<html>\n"; 
+echo "<body>\n"; 
+echo "<h1> Work in Progress </h1>\n"; 
+echo "This is still a work in progress. Please check back in July 20th 2014 <br> - Jeremiah ONeal <br>\n"; 
+echo "</body>\n"; 
+echo "</html>\n";
+?>
